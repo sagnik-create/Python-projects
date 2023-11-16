be1834d8ed1558f7bc1cae464b33e7ad3c2979ce
Sagnik Dey <146818206+sagnik-create@users.noreply.github.com>
2023-11-16 14:50:40 +0530

    My First Automation projects
    This is a simple Audiobook converter, that converts any pdf file to an mp3 file

---
+++ Text to speech.py
@@ -0,0 +1,15 @@
+import pyttsx3,PyPDF2
+
+#insert name of your pdf 
+pdfreader = PyPDF2.PdfReader('b-l4s5i-iot01a (1).pdf', 'rb')
+speaker = pyttsx3.init()
+
+for page_num in range(len(pdfreader.pages)):
+    text = pdfreader.pages[page_num].extract_text()
+    clean_text = text.strip().replace('\n', ' ')
+    print(clean_text)
+#name mp3 file whatever you would like
+speaker.save_to_file(clean_text, 'story.mp3')
+speaker.runAndWait()
+
+speaker.stop()
